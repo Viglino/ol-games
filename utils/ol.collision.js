3907a1bbba1f22c5aed9407836b09639be3dc2fe
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-03-18 17:22:19 +0100

    [INIT] Collisions

---
+++ utils/ol.collision.js
@@ -0,0 +1,96 @@
+﻿
+ol.Collision = function (options)
+{	
+	ol.Object.call(this);
+
+	this.map = options.map;
+	var self = this;
+	// Save current info
+	this.map.on("precompose", function (e)
+	{	self.e = e;
+		self.frameState = e.frameState;
+	});
+
+	this.resample = options.resample || 1;
+	// Collision canvas
+	this.canvas = document.createElement('canvas');
+};
+ol.inherits (ol.Collision, ol.Object);
+
+/** Get image used to test the collision
+*/
+ol.Collision.prototype.getImage = function ()
+{	return this.canvas;
+}
+
+/** Test if a sprite goes out of the current extent
+* @param {ol.Sprite} s1 the sprite to test
+* @return {N|S|E|W|false} the direction or false if 
+*/
+ol.Collision.prototype.overflow = function (s1)
+{	if (!this.frameState) return false;
+	var e = this.frameState.extent;
+	var es = s1.getBBox(this.frameState.viewState.resolution);
+	if (e[0]>es[0]) return "E";
+	if (e[1]>es[1]) return "N";
+	if (e[2]<es[2]) return "W";
+	if (e[3]<es[3]) return "S";
+	return false;
+};
+
+ol.Collision.prototype.getPixel = function (p)
+{	var m = this.frameState.coordinateToPixelTransform;
+	return [ m[0]*p[0] + m[1]*p[1] +m[4], m[2]*p[0] + m[3]*p[1] +m[5] ];
+};
+
+/** Test collision beetween 2 sprites
+* @param {ol.Sprite} s1 first sprite
+* @param {ol.Sprite} s2 second sprite
+* @return {ol.coordinate | false} false if no collision detected, coordinate of the hit point
+*/
+ol.Collision.prototype.test = function (s1, s2)
+{	if (!this.frameState) return false;
+	// Intersect extent
+	var e1 = s1.getBBox(this.frameState.viewState.resolution);
+	var e2 = s2.getBBox(this.frameState.viewState.resolution);
+	if (!ol.extent.intersects(e1,e2)) return false;
+	// Transform pixel to 
+	p1 = this.getPixel([e1[0],e1[1]]);
+	p2 = this.getPixel([e2[0],e2[1]]);
+	// Compose mage in a collision canvas
+	var fac = this.resample;
+	var s = Math.trunc(s1.getImage().size * s1.getImage().getScale() /fac);
+	var c = this.canvas;
+	c.width = c.height = s;
+	var ctx = c.getContext("2d");
+	ctx.save();
+		ctx.globalCompositeOperation="copy";
+		ctx.fillStyle="#FFF";
+		ctx.fillRect(0,0,s,s);
+		// Blit sprite 1
+		ctx.globalCompositeOperation="destination-out";
+		ctx.drawImage (s1.getImage().getImage(), 0, 0, s1.getImage().size, s1.getImage().size, 0, 0,s,s);
+		// Blit sprite 2
+		ctx.globalCompositeOperation="source-out";
+		ctx.translate ((p2[0]-p1[0])/fac, (p2[1]-p1[1])/fac);
+		ctx.drawImage (s2.getImage().getImage(), 0, 0, s2.getImage().size, s2.getImage().size, 0, 0,s,s);
+	/*	
+	ctx.globalCompositeOperation="source-out";
+		ctx.translate ((p2[0]-p1[0])/fac, (p2[1]-p1[1])/fac);
+		ctx.drawImage (s2.getImage().getImage(), 0, 0, s2.getImage().size, s2.getImage().size, 0, 0,s,s);
+		ctx.fillStyle="#FFF";
+		ctx.fillRect(0,0,s,s);
+		*/
+	ctx.restore();
+
+	var imgdata = ctx.getImageData(0,0,s,s);
+				
+	for (var j=3; j<imgdata.data.length; j+=4)
+	{	if (imgdata.data[j]>0)
+		{	var y = (j/4/s)|0;
+			var x = (j/4 - y*s)|0;
+			return [x*fac,y*fac];
+		}	
+	}
+
+};
---
+++ utils/ol.geom.utils.js
@@ -0,0 +1,165 @@
+﻿/** Project a coordinate on a line and get linear ref
+* @param { ol.Coordinate } c the point to project
+* @param { Array<ol.Coordinate> } seg a segment: 2 coordinate array
+* @return { number } linear ref on the segment (0: first point, 1: last point)
+*/
+ol.coordinate.projLineRef = function (c, seg)
+{	var A = c[0]-seg[0][0];
+	var B = c[1]-seg[0][1];
+	var C = seg[1][0]-seg[0][0];
+	var D = seg[1][1]-seg[0][1];
+
+	var det = A*C + B*D;
+	var len = C*C + D*D;
+
+	if (len!=0) return det/len;
+	else return 0;
+};
+
+/** Project a coordinate on a line
+* @param { ol.Coordinate } c the point to project
+* @param { Array<ol.Coordinate> } seg a segment: 2 coordinate array
+* @return { ol.Coordinate } the projected point
+*/
+ol.coordinate.projLine = function (c, seg)
+{	var s = ol.coordinate.projLineRef(c, seg);
+	return [seg[0][0] + s*(seg[1][0]-seg[0][0]), seg[0][1] + s*(seg[1][1]-seg[0][1])];
+};
+
+/** Project a coordinate on a segment
+* @param { ol.Coordinate } c the point to project
+* @param { Array<ol.Coordinate> } seg a segment: 2 coordinate array
+* @return { ol.Coordinate } the projected point
+*/
+ol.coordinate.projSeg = function (c, seg)
+{	var s = ol.coordinate.projLineRef(c, seg);
+	if (s <= 0) return seg[0];
+	else if (s >= 1) return seg[1];
+	else return [seg[0][0]+s*(seg[1][0]-seg[0][0]), seg[0][1]+s*(seg[1][1]-seg[0][1])];
+};
+
+/** Reverses a list of coordinates
+*/
+ol.coordinate.revers = function (c)
+{	var c2 = [];
+	for (var i = -1; i>=0; i--)
+	{	c2.push(c[i]);
+	}
+};
+
+/** Get coordinate on the line at a ref
+* @param {number} r the ref
+* @param {Array<ol.coordinate>} seg current segment
+* @retun {coord} coordinate
+*/
+ol.geom.LineString.prototype.getCoordinateAtRef = function (r, seg)
+{	if (r < 1e-10)
+	{	if (seg) 
+		{	var c = this.getCoordinates();
+			seg[0] = c[0];
+			seg[1] = c[1];
+		}
+		return this.getFirstCoordinate();
+	}
+	if (this.getLength()-r < 1e-10) 
+	{	if (seg) 
+		{	var c = this.getCoordinates();
+			seg[0] = c[c.length-2];
+			seg[1] = c[c.length-1];
+		}
+		return this.getLastCoordinate();
+	}
+	if (!seg) seg=[];
+	var s = 0;
+	var coord = this.getCoordinates();
+	for (var i=1; i<coord.length; i++)
+	{	var d = ol.coordinate.dist2d(coord[i-1], coord[i]);
+		if (s+d >= r)
+		{	var p0 = seg[0] = coord[i-1];
+			var p1 = seg[1] = coord[i];
+			var d = ol.coordinate.dist2d(p0,p1)
+			return [
+				p0[0] + (r-s) * (p1[0]-p0[0]) /d,
+				p0[1] + (r-s) * (p1[1]-p0[1]) /d
+			];
+		}
+		s += d;
+	}
+};
+
+/** Get the linear reference of a point along the linestring
+* @param {ol.Coordinate} p coordinate to get ref
+* @retun {number|false} linear ref, false if p is not on the linestring
+*/
+ol.geom.LineString.prototype.lineRef = function (p)
+{	var p0, p1, s;
+	var coord = this.getCoordinates();
+	var s0 = 0;
+	for (var i=1; i<coord.length; i++)
+	{	p0 = coord[i-1];
+		p1 = coord[i];
+		s = ol.coordinate.projLineRef(p,[p0,p1]);
+		if (s>=0 && s<=1)
+		{	var pi = [p0[0] + s*(p1[0]-p0[0]), p0[1] + s*(p1[1]-p0[1])];
+			if (ol.coordinate.dist2d(p,pi) < 1e-10)
+			{	return s0 + s*ol.coordinate.dist2d(p0, p1);
+			}
+		}
+		else
+		{	s0 += ol.coordinate.dist2d(p0,p1);
+		}
+	}
+	// Point is not on the linestring
+	return false;
+};
+
+/** Samples a line with a set of points
+* @param {Number} dl sample distance
+* @param {} options 
+*	- minLength {Number} line with a length underneath this distance will return an empty array, default 0
+*	- distribute {boolean} tries to distribute the points along the line, default false
+*	- center {boolean} center points on the line, if false the first point start on the first point of the line, default false
+*/
+ol.geom.LineString.prototype.sample = function (dl, options)
+{	options = options || {};
+	var g = this.getCoordinates();
+	var coords = [];
+	var p0, p1, s, p, dist, a=0, r=0, l=0;
+	var d0 = this.getLength();
+	if (d0 < (options.minLength||0)) return [];
+	// distribute
+	if (options.distribute)
+	{	d0 = this.getLength();
+		if (d0>dl) dl = d0 / ((d0/dl)|0);
+		else dl = d0;
+	}
+	// Center
+	if (options.center)
+	{	a = (d0 - dl * ((d0/dl)|0));
+		if (a < 1e-10) 
+		{	if (d0 > dl) a = -dl/2;
+			else 
+			{	dl = d0;
+				a = -d0/2;
+			}
+		}
+		else a = a/2 -dl;
+	}
+	else
+	{	coords.push(g[0]);
+	}
+	// Linear interpol
+	for (var i=1; i<g.length; i++)
+	{	p0 = g[i-1];
+		p1 = g[i];
+		dist = ol.coordinate.dist2d(p0,p1);
+		for (var k = dl + a-l; k<dist; k+=dl)
+		{	s = k / dist;
+			p = [ p0[0] + (p1[0]-p0[0])*s, p0[1] + (p1[1]-p0[1])*s];
+			coords.push(p);
+			a += dl;
+		}
+		l += dist;
+	}
+	return coords;
+}
