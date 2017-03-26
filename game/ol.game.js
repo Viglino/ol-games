355614fd28cad3801859db347f41bfe48c7b222f
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-03-26 19:18:12 +0200

    [ADD] game

---
+++ game/ol.game.js
@@ -0,0 +1,128 @@
+﻿/*	Copyright (c) 2017 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+	
+	@example http://www.hexographer.com/
+	
+*/
+/**
+* Game class
+*
+* @constructor ol.Game
+* @extends {ol.Object}
+* @param {olx.Game=} options
+* @todo 
+*/
+ol.Game = function(options)
+{	options = options || {};
+	
+	ol.Object.call (this);
+
+	var map = options.map || new ol.Map
+			({	target: options.target,
+				loadTilesWhileAnimating: true,
+				loadTilesWhileInteracting: true,
+				view: new ol.View
+				({	zoom: options.zoom,
+					center: options.center
+				}),
+				interactions: [],
+				controls: [],
+				layers: options.layers,
+			});
+	// Set the map and the game loop (postcompose hook)
+	this.setMap(map);
+	this.pause_ = true;
+
+	this.collisions = [];
+	
+	this.collision = new ol.Collision({ game: this, resample: options.collisionResample });
+
+};
+ol.inherits (ol.Game, ol.Object);
+
+/**	Set the game map
+*/
+ol.Game.prototype.setMap = function(map)
+{	if (this.map)
+	{	this.map.un("postcompose", this.anim_, this);
+	}
+	this.map = map;
+	if (this.map)
+	{	this.map.on ("postcompose", this.anim_, this);
+	}
+};
+
+/**	Get the game map
+*/
+ol.Game.prototype.getMap = function()
+{	return this.map;
+};
+
+/**	Get the game view
+*/
+ol.Game.prototype.getView = function()
+{	return this.map.getView();
+};
+
+/**	Add control to th map
+*/
+ol.Game.prototype.addControl = function(c)
+{	return this.map.addControl(c);
+};
+
+/**	Test collision on the map
+*/
+ol.Game.prototype.collide = function(s1,s2)
+{	return this.collision.collide(s1,s2);
+}
+
+/**	Start the game
+*/
+ol.Game.prototype.start = function()
+{	this.time = (new Date()).getTime();
+	this.pause_ = false;
+	this.map.render();
+	this.dispatchEvent({ type:'start' });
+};
+
+/**	Pause the game
+*/
+ol.Game.prototype.pause = function()
+{	this.pause_ = true;
+	this.dispatchEvent({ type:'pause' });
+};
+
+/**	Is the game paused
+*/
+ol.Game.prototype.paused = function()
+{	return this.pause_;
+};
+
+/** Add new collision to the game
+*/
+ol.Game.prototype.addCollision = function(collision)
+{	if (!collision instanceof Array) collision = [collision];
+	this.collisions = this.collisions.concat(collision);
+};
+
+/**	Main game loop
+*/
+ol.Game.prototype.anim_ = function(e)
+{	e.dt = e.frameState.time - this.time;
+	this.time = e.frameState.time;
+	this.frameState = e.frameState;
+	if (!this.pause_)
+	{	// Test collisions
+		for (var i=this.collisions.length-1; i>=0; i--) 
+		{	this.collisions[i].dispatch();
+		}
+
+		// Render the game
+		this.dispatchEvent({ type:"render", context:e.context, dt:e.dt, frameState:e.frameState, vectorContext:e.vectorContext });
+
+		// Continue animation
+		this.map.render();
+	}
+};
+
---
+++ game/ol.offscreen.js
@@ -0,0 +1,150 @@
+﻿/*	Copyright (c) 2017 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+	
+	@example http://www.hexographer.com/
+	
+*/
+/**
+* Offscrenn map use to perform collision test
+*
+* @constructor ol.Game
+* @extends {ol.Object}
+* @param {olx.Graph=} options
+*	- map {ol.Map} the map you want to synchronize to
+*	- layers {Array<ol.layer>} an array of layers to use
+* @todo 
+*/
+ol.Offscreen = function(options)
+{	options = options || {};
+	ol.Object.call (this);
+
+	// Map element
+	var odiv = this.element = document.createElement('div');
+	odiv.style.position = "absolute";
+	odiv.style.opacity = 0;
+	odiv.style.visibility = "hidden";
+	odiv.style.top = odiv.style.left = "-10000px";
+	odiv.style.width = map.getSize()[0]+"px";
+	odiv.style.height = map.getSize()[1]+"px";
+	odiv.className = "ol-games-offscreen";
+	
+	// Offscreen map
+	this.offmap = new ol.Map (
+		{	target: odiv,
+			// loadTilesWhileAnimating: true,
+			controls: [],
+			interactions: [],
+			layers: options.layers
+		});
+	map.on("change:size", function(e){ 
+		odiv.style.width = map.getSize()[0]+"px";
+		odiv.style.height = map.getSize()[1]+"px";
+	});
+	this.setMap(options.map);
+
+	// Resample to test collision
+	this.resample = options.resample || 1;
+
+	// Canvas
+	this.image = odiv.children[0].children[0];
+	
+	this.canvas = document.createElement('canvas');
+
+};
+ol.inherits (ol.Offscreen, ol.Object);
+
+/**	Set the game map
+*/
+ol.Offscreen.prototype.setMap = function(map)
+{	if (this.map)
+	{	this.map.un("change:size", this.changeSize_, this);
+		this.map.getViewport().removeChild(this.element);
+		this.offmap.setView (null);
+	}
+	this.map = map;
+	if (this.map)
+	{	this.map.getViewport().appendChild(this.element);
+		this.map.on("change:size", this.changeSize_, this);
+		this.offmap.setSize (this.map.getSize());
+		this.offmap.setView (this.map.getView());
+	}
+};
+
+/** Get offscreen image
+*/
+ol.Offscreen.prototype.getImage = function()
+{	return this.image;
+};
+
+/** Change size
+*/
+ol.Offscreen.prototype.changeSize_ = function()
+{	this.element.style.width = this.map.getSize()[0]+"px";
+	this.element.style.height = this.map.getSize()[1]+"px";
+};
+
+/** GetPixel value
+* @param {ol.Pixel} pixel
+* @return {Array} an array representing [R, G, B, A] pixel values (0 - 255) or null if no pixel
+*/
+ol.Offscreen.prototype.getPixelValue = function(pixel)
+{	var val = null;
+	this.offmap.forEachLayerAtPixel(pixel, function(l, c){ if (l && c) { val = c; return true; } });
+	return val;
+};
+
+/** GetPixel value at a coordinate
+* @param {ol.Coordinate} pixel
+* @return {Array} an array representing [R, G, B, A] pixel values (0 - 255) or null if no pixel
+*/
+ol.Offscreen.prototype.getValue = function(coord)
+{	return this.getPixelValue ( this.offmap.getPixelFromCoordinate(coord) );
+};
+
+/** Test if a sprite collide
+* @param {ol.Sprite} s1 sprite to test collision
+* @return {ol.coordinate | false} false if no collision detected, coordinate of the hit point
+*/
+ol.Offscreen.prototype.collide = function(s1)
+{	var ratio = map.get('pixelRatio') || window.devicePixelRatio;
+
+	// Intersect extent
+	var e1 = s1.getBBox(this.map.getView().getResolution());
+	var e2 = this.map.getView().calculateExtent(this.map.getSize());
+	if (!ol.extent.intersects(e1,e2)) return false;
+
+	// Transform to pixel
+	var p1 = this.map.getPixelFromCoordinate(e1);
+	var p2 = this.map.getPixelFromCoordinate(e2);
+
+	// Compose image in a collision canvas
+	var fac = this.resample;
+	var s = Math.trunc(s1.getImage().size * s1.getImage().getScale() /fac);
+	var c = this.canvas;
+	c.width = c.height = s;
+	var ctx = c.getContext("2d");
+	ctx.save();
+		ctx.globalCompositeOperation="copy";
+		ctx.fillStyle="#FFF";
+		ctx.fillRect(0,0,s,s);
+		// Blit sprite 1
+		ctx.globalCompositeOperation="destination-out";
+		ctx.drawImage (s1.getImage().getImage(), 0, 0, s1.getImage().size, s1.getImage().size, 0, 0,s,s);
+		// Blit sprite 2
+		ctx.globalCompositeOperation="source-out";
+		ctx.drawImage (this.getImage(), p1[0]*ratio, (p1[1] - s*fac)*ratio, s*fac*ratio,s*fac*ratio, 0, 0,s,s);
+	ctx.restore();
+
+	var imgdata = ctx.getImageData(0,0,s,s);
+				
+	for (var j=3; j<imgdata.data.length; j+=4)
+	{	if (imgdata.data[j]>0)
+		{	var y = (j/4/s)|0;
+			var x = (j/4 - y*s)|0;
+			return [x*fac,y*fac];
+		}	
+	}
+
+	return false;
+};
